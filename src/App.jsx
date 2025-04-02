986167468e1b5fbdd2ed1d04dc0070d5177eed42
Sara Moss <moss.s.m@wustl.edu>
2025-04-01 19:57:12 -0500

    Xs and Os added

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ import './App.css'
 // export default App
 
 
-function Square({ value, onSquareClick }) {
+function Square({value, onSquareClick}) {
   return (
     <button className="square" onClick={onSquareClick}>
       {value}
@@ -44,12 +44,21 @@ function Square({ value, onSquareClick }) {
 }
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
+    if (squares[i]) {
+      return;
+    }
     const nextSquares = squares.slice();
-    nextSquares[i] = 'X';
+    if (xIsNext) {
+      nextSquares[i] = 'X';
+    } else {
+      nextSquares[i] = 'O';
+    }
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return (
@@ -72,3 +81,4 @@ export default function Board() {
     </>
   );
 }
+

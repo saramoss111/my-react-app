20b166c106c49d773d6df041040a4f50b2b457ac
Sara Moss <moss.s.m@wustl.edu>
2025-04-01 20:06:51 -0500

    Browser is failing...

--- src/App.jsx
+++ src/App.jsx
@@ -47,13 +47,19 @@ export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
-  function handleClick(i) {
-    if (squares[i] || calculateWinner(squares)) {
-      return;
-    }
-    const nextSquares = squares.slice();
+  if (calculateWinner(squares) || squares[i]) {
+    return;
   }
-  
+  const nextSquares = squares.slice();
+  if (xIsNext) {
+    nextSquares[i] = 'X';
+  } else {
+    nextSquares[i] = 'O';
+  }
+  setSquares(nextSquares);
+  setXIsNext(!xIsNext);
+
+
   return (
     <>
       <div className="board-row">
